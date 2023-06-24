5304ff519278e82904dd09a75e3f43541230505f
Saurabh Singh Bisht <bisht.singh750@gmail.com>
2023-06-24 15:32:43 +0530

    get changed to is in Payment

--- src/main/java/com/driver/model/Payment.java
+++ src/main/java/com/driver/model/Payment.java
@@ -31,7 +31,7 @@ public class Payment {
         this.id = id;
     }
 
-    public Boolean getPaymentCompleted() {
+    public Boolean isPaymentCompleted() {
         return paymentCompleted;
     }
 
